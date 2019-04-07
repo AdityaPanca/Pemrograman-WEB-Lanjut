139de64243c6da859c577843fddb87fd58215660
AdityaPanca <aditya.annecdo@gmail.com>
2019-04-07 17:57:38 +0700

    templatedriven

---
+++ 07. TemplateDriven/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReactiveFormComponent,
+    TemplateDrivenComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
