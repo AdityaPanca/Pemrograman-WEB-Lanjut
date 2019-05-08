bd3586a140db7d2dfef8117384df5ae758002a86
AdityaPanca <aditya.annecdo@gmail.com>
2019-05-08 14:37:17 +0700

    gitbook routes

---
+++ 11. Authentication/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from "@angular/http";
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { FormMemberComponent } from './form-member/form-member.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsComponent,
+    FormMemberComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    HomeComponent,
+    ProfileComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {path:'',component:HomeComponent},
+      {path:'form', component:FormMemberComponent},
+      {path:'post', component:PostsComponent},
+      {path:'profile/:id', component:ProfileComponent},
+      {path:'**', component:NotFoundComponent}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
